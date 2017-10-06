c7e8671d5f178ce43e7e930250763fd1c0b1bf21
etheryli <gd0697@gmail.com>
2017-10-06 02:55:40 -0400

    initial film abstract class

---
+++ film.cpp
@@ -0,0 +1,13 @@
+/*
+ * Created by Hung Nguyen
+ * Student ID: 905925175
+ * Email: dnhung7@vt.edu
+ * Class: ECE 3574
+ * Assignment: Homework 2
+ * File: Abstract header file for Film class
+ */
+
+#include "film.h"
+
+Film::Film() : m_FilmID(title), m_Title(title), m_Director(dir),
+    m_FilmLength(length), m_ReleaseDate(relDate) {}
---
+++ film.h
@@ -0,0 +1,38 @@
+/*
+ * Created by H u N g
+ * Student ID: 905******
+ * Email: ******@vt.edu
+ * Class: ECE ****
+ * Assignment: Homework 2
+ * File: Abstract header file for Film class
+ */
+
+#ifndef FILM_H
+#define FILM_H
+
+#include <QDate>
+#include <QString>
+
+class Film
+{
+public:
+    // Virtual Destructor for base class
+    virtual ~Film();
+
+    // Constructor with private members set
+    Film(QString id, QString title, QString dir,
+         quint32 length, QDate relDate);
+
+    // Virtual toString
+    virtual toString(bool labeled, QString sepchar);
+
+private:
+    // Private members for a Film
+    QString m_FilmID;
+    QString m_Title;
+    QString m_Director;
+    quint32 m_FilmLength;
+    QDate m_ReleaseDate;
+};
+
+#endif // FILM_H
